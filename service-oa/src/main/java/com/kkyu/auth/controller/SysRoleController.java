1d9997328ed703bd93d4be50aa71eba102e67c07
往昔yu <kkikikkxx@gmail.com>
2024-12-23 10:15:21 +0800

    完成角色管理的增删改

--- service-oa/src/main/java/com/kkyu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/kkyu/auth/controller/SysRoleController.java
@@ -8,10 +8,7 @@ import com.kkyu.model.system.SysRole;
 import com.kkyu.vo.system.SysRoleQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,6 +42,11 @@ public class SysRoleController {
 
     /**
      * 条件分页查询
+     *
+     * @param page           分页参数，指定页码
+     * @param limit          分页参数，指定每页记录数
+     * @param sysRoleQueryVo 查询条件封装对象
+     * @return 分页查询结果
      */
     @GetMapping("{page}/{limit}")
     public Result<Page<SysRole>> pageQueryRole(
@@ -56,14 +58,70 @@ public class SysRoleController {
         Page<SysRole> pageParams = new Page<>(page, limit);
         QueryWrapper<SysRole> wrapper = new QueryWrapper<>();
         String roleName = sysRoleQueryVo.getRoleName();
-        if (!StringUtils.isEmpty(roleName)){
+        if (!StringUtils.isEmpty(roleName)) {
             // 封装
-            wrapper.like("role_name",roleName);
+            wrapper.like("role_name", roleName);
         }
 
         Page<SysRole> pageData = sysRoleService.page(pageParams, wrapper);
 
         return Result.successData(pageData);
+    }
+
+    /**
+     * 添加角色
+     *
+     * @param role 要添加的角色信息
+     * @return 添加结果
+     */
+    @PostMapping("save")
+    public Result<String> addRole(@RequestBody SysRole role) {
+        return sysRoleService.save(role) ? Result.successMsg("添加成功") : Result.error("添加失败");
+    }
+
+    /**
+     * 根据ID查询角色信息。
+     *
+     * @param id 角色的唯一标识符
+     * @return 包含查询结果的Result对象，其中data字段为SysRole对象
+     */
+    @GetMapping("get/{id}")
+    public Result<SysRole> get(@PathVariable Long id) {
+        SysRole sysRole = sysRoleService.getById(id);
+        return Result.successData(sysRole);
+    }
+
+    /**
+     * 修改指定的角色信息。
+     *
+     * @param role 包含更新信息的角色对象
+     * @return 包含操作结果信息的Result对象
+     */
+    @PutMapping("update")
+    public Result<String> updateById(@RequestBody SysRole role) {
+        return sysRoleService.updateById(role) ? Result.successMsg("修改成功") : Result.error("修改失败");
+    }
+
+    /**
+     * 根据ID删除角色。
+     *
+     * @param id 要删除的角色的唯一标识符
+     * @return 包含操作结果信息的Result对象
+     */
+    @DeleteMapping("remove/{id}")
+    public Result<String> remove(@PathVariable Long id) {
+        return sysRoleService.removeById(id) ? Result.successMsg("删除成功") : Result.error("删除失败");
+    }
+
+    /**
+     * 批量删除多个角色。
+     *
+     * @param idList 包含要删除的角色ID列表
+     * @return 包含操作结果信息的Result对象
+     */
+    @DeleteMapping("batchRemove")
+    public Result<String> batchRemove(@RequestBody List<Long> idList) {
+        return sysRoleService.removeByIds(idList) ? Result.successMsg("删除成功") : Result.error("删除失败");
     }
 
 }
