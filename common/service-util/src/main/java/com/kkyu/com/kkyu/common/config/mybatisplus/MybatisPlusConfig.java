9b63d013a9e44f59ebec5c349579df63191cca28
往昔yu <kkikikkxx@gmail.com>
2024-12-23 09:37:40 +0800

    条件分页查询

---
+++ common/service-util/src/main/java/com/kkyu/com/kkyu/common/config/mybatisplus/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.kkyu.com.kkyu.common.config.mybatisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@MapperScan("com.kkyu.auth.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,不需要设置 MybatisConfiguration#useDeprecatedExecutor = false
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
--- service-oa/src/main/java/com/kkyu/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/kkyu/auth/controller/SysRoleController.java
@@ -1,16 +1,24 @@
 package com.kkyu.auth.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kkyu.auth.service.SysRoleService;
 import com.kkyu.common.result.Result;
 import com.kkyu.model.system.SysRole;
+import com.kkyu.vo.system.SysRoleQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+/**
+ * 角色管理接口
+ * 提供角色相关的操作，如查询所有角色等
+ */
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -23,12 +31,39 @@ public class SysRoleController {
         this.sysRoleService = sysRoleService;
     }
 
-    // 查询所有的角色
+    /**
+     * 查询所有角色
+     *
+     * @return 角色列表
+     */
     @GetMapping("/findAll")
     public Result<List<SysRole>> findAll() {
         List<SysRole> list = sysRoleService.list();
 
         return Result.successData(list);
+    }
+
+    /**
+     * 条件分页查询
+     */
+    @GetMapping("{page}/{limit}")
+    public Result<Page<SysRole>> pageQueryRole(
+            @PathVariable Long page,
+            @PathVariable Long limit,
+            SysRoleQueryVo sysRoleQueryVo
+    ) {
+
+        Page<SysRole> pageParams = new Page<>(page, limit);
+        QueryWrapper<SysRole> wrapper = new QueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if (!StringUtils.isEmpty(roleName)){
+            // 封装
+            wrapper.like("role_name",roleName);
+        }
+
+        Page<SysRole> pageData = sysRoleService.page(pageParams, wrapper);
+
+        return Result.successData(pageData);
     }
 
 }
